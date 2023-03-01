6ef666fd7f5fb5f84724c4767a4908ac17a2fde9
Chris Abdo <chrisabdo@Chriss-Mac-mini.local>
2023-02-28 21:47:57 -0500

    ADDED: styling changes

--- src/pages/commend.tsx
+++ src/pages/commend.tsx
@@ -468,13 +468,20 @@ export default function Discover() {
                                     </div>
                                   </div>
                                   <div className="mt-5 sm:mt-6">
-                                    <button
+                                    {/* <button
                                       type="button"
                                       className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
                                       onClick={() => setSelectedNFT(null)}
                                     >
                                       Go back to dashboard
-                                    </button>
+                                    </button> */}
+                                    <Button
+                                      onClick={() => setSelectedNFT(null)}
+                                      variant="default"
+                                      className="inline-flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:text-sm"
+                                    >
+                                      Close
+                                    </Button>
                                   </div>
                                 </Dialog.Panel>
                               </Transition.Child>
@@ -561,7 +568,7 @@ export default function Discover() {
                                       </div>
                                     </div>
                                   </div>
-                                  <div className="flex mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+                                  <div className="flex space-x-2 mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
                                     {/* <button
                                       type="button"
                                       className="inline-flex w-full justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
@@ -593,22 +600,22 @@ export default function Discover() {
                                       <Button
                                         variant="default"
                                         onClick={() => handleGiveHeat(nft)}
-                                        className="inline-flex w-full justify-center rounded-md border border-transparent  px-4 py-2 text-base font-medium  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm"
+                                        className="inline-flex w-full justify-center rounded-md border border-transparent  px-4 py-2 text-base font-medium  focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-2 sm:text-sm lg:mt-0 mt-3"
                                       >
-                                        Submit Commend
+                                        Submit
                                       </Button>
                                     )}
 
-                                    <button
-                                      type="button"
-                                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
+                                    <Button
+                                      variant="outline"
                                       onClick={() =>
                                         setSelectedNFTCommends(null)
                                       }
                                       ref={cancelButtonRef}
+                                      className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:col-start-1 sm:mt-0 sm:text-sm"
                                     >
-                                      Cancel
-                                    </button>
+                                      Close
+                                    </Button>
                                   </div>
                                 </Dialog.Panel>
                               </Transition.Child>
